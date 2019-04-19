7273c42f29dab1eadfad9858afc1e143ffcc0786
Jon Badiali <jon@jonbadiali.com>
2019-04-19 12:42:21 -0400

    Added tests for send_message.c, including SSL tests

--- src/rocto/send_message.c
+++ src/rocto/send_message.c
@@ -18,7 +18,9 @@
 #include <string.h>
 #include <errno.h>
 #include <assert.h>
+
 #include <openssl/ssl.h>
+#include <openssl/err.h>
 
 // Used to convert between network and host endian
 #include <arpa/inet.h>
@@ -27,7 +29,9 @@
 #include "message_formats.h"
 
 int send_message(RoctoSession *session, BaseMessage *message) {
-	int result;
+	int result = 0, ossl_error = 0;
+	unsigned long ossl_error_code = 0;
+	char *err = NULL;
 
 	TRACE(ERR_ENTERING_FUNCTION, "send_message");
 	TRACE(ERR_SEND_MESSAGE, message->type, ntohl(message->length));
@@ -37,20 +41,15 @@ int send_message(RoctoSession *session, BaseMessage *message) {
 		result = SSL_write(session->ossl_connection, (char*)message, ntohl(message->length) + 1);
 		if (result <= 0 ) {
 			ossl_error = SSL_get_error(session->ossl_connection, result);
-			// Using blocking I/O - this error should never happen
-			if (ossl_error == SSL_ERROR_WANT_WRITE) {
-				ossl_error_code = ERR_peek_last_error();
-				err = ERR_error_string(ossl_error_code, err);
-				FATAL(ERR_ROCTO_OSSL_WRITE_FAILED, err);
+			if(errno == ECONNRESET) {
+				return 1;
 			}
-			if (ossl_error == SSL_ERROR_SYSCALL) {
-				if(errno == ECONNRESET)
-					return 1;
+			else if (ossl_error == SSL_ERROR_SYSCALL) {
 				ossl_error_code = ERR_peek_last_error();
 				err = ERR_error_string(ossl_error_code, err);
 				FATAL(ERR_SYSCALL, "unknown (OpenSSL)", errno, strerror(errno));
 			}
-			if (ossl_error == SSL_ERROR_SSL) {
+			else {
 				ossl_error_code = ERR_peek_last_error();
 				err = ERR_error_string(ossl_error_code, err);
 				FATAL(ERR_ROCTO_OSSL_WRITE_FAILED, err);
