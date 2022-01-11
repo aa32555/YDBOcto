8845cbbe37690ba489b35940bbb7c18108c7fac6
Ganesh Mahesh <ganesh@yottadb.com>
2022-01-11 14:02:20 -0500

    [#767] Issue ERR_GROUP_BY_OR_AGGREGATE_FUNCTION error even if GROUP BY in sub query uses outer query columns
    As an optimization an outer query column in GroupBy of inner query is removed. In the case where only one column is present it becomes an empty GroupBy. Before this change such a case would work as if no GroupBy was used. But now we set `aggregate_function_or_group_by_specified` of the SqlTableAlias to TRUE indicating that the GroupBy exists. This enables GroupBy validations to happen during query qualification resulting in appropriate error being issued when non grouped columns are used in other clauses.
    
    **Testing**
    * TGB15 is added to validate this implementation

---
+++ tests/fixtures/TGB15.sql
@@ -0,0 +1,16 @@
+#################################################################
+#								#
+# Copyright (c) 2022 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+
+-- TGB15 : OCTO765 : Issue ERR_GROUP_BY_OR_AGGREGATE_FUNCTION error even if GROUP BY in sub query uses outer query columns
+
+SELECT id,firstname FROM names n1 WHERE id IN (SELECT n2.id FROM names n2 group by n1.id);
+SELECT id,firstname FROM names n1 HAVING 1 IN (SELECT n2.id FROM names n2 group by n1.id);
